94fa77228518f90eee1dbe40d9381b00ee8b2f0b
ikhanage <quiteimran@yahoo.co.uk>
2016-07-12 19:29:31 +0100

    Add project files.

---
+++ ShowMeTheTickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShowMeTheTickets.Startup))]
+namespace ShowMeTheTickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
