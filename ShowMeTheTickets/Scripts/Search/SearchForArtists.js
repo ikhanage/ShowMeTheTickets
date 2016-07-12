ff69362ad5ab7a6d6a73699ca26ef13e1fd08005
ikhanage <quiteimran@yahoo.co.uk>
2016-07-12 23:59:36 +0100

    Added some ko bindings.

--- ShowMeTheTickets/Scripts/Search/SearchForArtists.js
+++ ShowMeTheTickets/Scripts/Search/SearchForArtists.js
@@ -1,1 +1,13 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/knockout/knockout.d.ts" />
+var SearchForArtists;
+(function (SearchForArtists) {
+    function SetBindings() {
+        ko.applyBindings(new Bindings());
+    }
+    SearchForArtists.SetBindings = SetBindings;
+    function Bindings() {
+        this.Artist = ko.observable();
+    }
+})(SearchForArtists || (SearchForArtists = {}));
 //# sourceMappingURL=SearchForArtists.js.map
--- ShowMeTheTickets/Scripts/Search/SearchForArtists.ts
+++ ShowMeTheTickets/Scripts/Search/SearchForArtists.ts
@@ -1,1 +1,12 @@
-﻿
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/knockout/knockout.d.ts" />
+
+module SearchForArtists {
+    export function SetBindings() {
+        ko.applyBindings(new Bindings());
+    }
+
+    function Bindings() {
+        this.Artist = ko.observable();
+    }
+}
