1e9c93b3ecc2ccf97407c1bf4950614cb4904709
ikhanage <quiteimran@yahoo.co.uk>
2016-07-17 22:17:16 +0100

    Removed some auto code

--- ShowMeTheTickets/Controllers/HomeController.cs
+++ ShowMeTheTickets/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using ShowMeTheTickets.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ShowMeTheTickets.Controllers
 {
@@ -12,20 +6,6 @@ namespace ShowMeTheTickets.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
